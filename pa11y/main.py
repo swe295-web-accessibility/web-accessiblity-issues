228557b749863c1e8ac626cacac9df518b5a9983
lori <11811407@mail.sustech.edu.cn>
2023-03-01 17:52:26 -0800

    debug pa11y script

--- pa11y/main.py
+++ pa11y/main.py
@@ -3,9 +3,9 @@ import subprocess
 
 def main(url: str, path: str):
     try:
-        subprocess.check_call("pa11y")
+        subprocess.check_call("pa11y", shell=True)
     except subprocess.CalledProcessError:
-        subprocess.run("npm install -g pa11y")
+        subprocess.run("npm install -g pa11y", shell=True)
     subprocess.run(f"pa11y --reporter csv {url} > {path}", shell=True)
 
 
